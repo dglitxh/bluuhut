ff2e7e99eeaaa8dc5d727b756e9b1b6b04b67cac
ydzly <ydglitch@gmail.com>
2024-02-05 13:01:32 +0000

    add: nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 "use client";
+import MainNav from "./components/nav";
 import { Providers } from "./providers";
 
 export default function Home() {
   return (
     <Providers>
-      <main
-        className={`flex min-h-screen flex-col items-center justify-between p-24`}
-      >
-        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-          Bluehut
+      <main className={` `}>
+        <div className="z-10 w-full items-center justify-between font-mono text-sm lg:flex">
+          <MainNav />
         </div>
       </main>
     </Providers>
