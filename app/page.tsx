ec3d5528753a0661f47799268a75f79f29e2a474
ydzly <ydglitch@gmail.com>
2024-02-05 12:16:29 +0000

    upd: rem providers

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
-import Image from "next/image";
+"use client";
+import { Providers } from "./providers";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        Bluehut
-      </div>
-    </main>
+    <Providers>
+      <main
+        className={`flex min-h-screen flex-col items-center justify-between p-24`}
+      >
+        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+          Bluehut
+        </div>
+      </main>
+    </Providers>
   );
 }
