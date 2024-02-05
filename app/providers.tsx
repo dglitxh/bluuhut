f1701b156620c38ef9beb082a78f891e97f146a3
ydzly <ydglitch@gmail.com>
2024-02-05 12:16:45 +0000

    upd: added next theme

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,14 +1,16 @@
 "use client";
 import { ColorModeContext, useColorMode } from "./utils/theme";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { NextUIProvider } from "@nextui-org/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const { colorModeCtx, mode }: any = useColorMode();
+  const colorModeCtx: any = useColorMode();
   return (
     <ColorModeContext.Provider value={colorModeCtx}>
       <NextUIProvider>
-        {children}
+        <NextThemesProvider attribute="class">{children}</NextThemesProvider>
       </NextUIProvider>
     </ColorModeContext.Provider>
   );
 }
+
