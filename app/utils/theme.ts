e514658ecc60ae16f656926c66acc6a327d0b957
ydzly <ydglitch@gmail.com>
2024-02-05 14:46:47 +0000

    upd: colors

--- app/utils/theme.ts
+++ app/utils/theme.ts
@@ -68,4 +68,23 @@ export const nextTheme = {
       },
     },
   },
+  light: {
+    colors: {
+      primary: {
+        50: "#3B096C",
+        100: "#520F83",
+        200: "#7318A2",
+        300: "#9823C2",
+        400: "#c031e2",
+        500: "#DD62ED",
+        600: "#F182F6",
+        700: "#FCADF9",
+        800: "#FDD5F9",
+        900: "#FEECFE",
+        DEFAULT: "#DD62ED",
+        foreground: "#ffffff",
+      },
+      focus: "#F182F6",
+    },
+  },
 };
