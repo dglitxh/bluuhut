8136245857b6b00716b9675380a0fa4c82cd913a
ydzly <ydglitch@gmail.com>
2024-02-06 14:55:31 +0000

    fix: hero width

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -3,12 +3,12 @@ import React from "react";
 
 export default function Hero() {
   return (
-    <div className="w-screen h-dvh flex items-center justify-center flex-col">
+    <div className="h-dvh flex items-center justify-center flex-col p-6">
       <h1 className="text-7xl">Hassle free solutions</h1>
       <br></br>
-      <h1 className="text-primary text-7xl"> at your doorstep </h1>
+      <h1 className="text-primary text-bold text-7xl"> at your doorstep </h1>
       <br></br>
-      <h3 className="text-xl">
+      <h3 className="text-xl text-bold">
         We deliver the most innovative and efficient technical solutions at a
         fast pace.
       </h3>
