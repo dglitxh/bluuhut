294c7b5d27d846b08d9be7d689317d8cd4f01632
akglitch <mcaninakwa@gmail.com>
2024-02-07 05:11:20 -0800

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Services from "./Services";
 export default function Home() {
   return (
     <main className={``}>
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="">
         <Services />
         <Team />
         <Footer />
