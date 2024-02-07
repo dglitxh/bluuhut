77e88c804d2625656d37b80f2395e0df559e1732
akglitch <mcaninakwa@gmail.com>
2024-02-07 05:05:46 -0800

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
+import Footer from "./Footer";
+import Team from "./Team";
+import Services from "./Services";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className={``}>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        Bluehut
+        <Services />
+        <Team />
+        <Footer />
       </div>
     </main>
   );
