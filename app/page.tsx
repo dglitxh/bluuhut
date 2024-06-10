087bd3afc798da87a5e36e4bea63412e9c1f1957
ydzly <ydglitch@gmail.com>
2024-06-09 21:46:23 -0400

    upd: cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,6 @@ import { Providers } from "./providers";
 import AboutUs from "./components/AboutUs";
 import Contact from "./components/Contact";
 import Pricing from "./components/Pricing";
-import "@fortawesome/fontawesome-svg-core/styles.css";
-import { config } from "@fortawesome/fontawesome-svg-core";
-config.autoAddCss = false;
 
 export default function Home() {
   return (
