a2588fde1a68cb58b7528b33245ee60efb737e08
ydzly <ydglitch@gmail.com>
2024-06-12 16:05:32 +0000

    upd: mailer

--- app/api/sendmail/route.ts
+++ app/api/sendmail/route.ts
@@ -1,40 +1,57 @@
-const Mailgun = require("mailgun-js");
-import { send } from "@emailjs/browser";
 import type { NextRequest, NextResponse } from "next/server";
 const mgKey = process.env.MAILGUN_API_KEY;
+const domainKey = process.env.MAILGUN_DOMAIN_KEY;
 const DOMAIN = "mail.bluehutsolutions.ca";
-const mg = Mailgun({
-  apiKey: mgKey,
-  domain: DOMAIN,
-});
+
+interface Form {
+  from: string;
+  to: string;
+  subject: string;
+  text: string;
+  html: string;
+}
 
 export async function POST(req: NextRequest, res: NextResponse) {
   let data = await req.json();
-  let resp;
-  let n_err: boolean = false;
-  console.log(data);
 
   const deets = {
     from: "Bluehut App <postmaster@mail.bluehutsolutions.ca>",
     to: "ydglitch@gmail.com",
-    subject: `Message from ${data.email} on BlueHut app`,
+    subject: `Message from ${data.firstName} on the BlueHut App`,
     text: `${data.message}`,
-    html: `<h1>${data.message}</h1>`,
+    html: `<h1>${data.message}<br></br><br></br><br></br>   From: ${data.email}</h1>`,
+  };
+
+  let form = new FormData();
+
+  for (let [key, value] of Object.entries(deets)) {
+    form.append(key, value);
+  }
+  console.log(form);
+
+  let config = {
+    method: "POST",
+    headers: {
+      // Accept: "multipart/form-data",
+      // "Content-Type": "multipart/form-data",
+      Authorization: "Basic " + Buffer.from(`api:${mgKey}`).toString("base64"),
+    },
+    body: form,
   };
 
   try {
-    const status = mg.messages().send(deets, function (error: any, body: any) {
-      console.log("sending.......");
-
-      if (error) {
-        console.log(error)
-        return;
-      }
-      console.log(body);
-      return body;
-    });
-    if (!status) throw new Error("message not sent");
-    else return Response.json({ msg: "email sent succesfully" });
+    let request = await fetch(
+      `https://api.mailgun.net/v3/${DOMAIN}/messages`,
+      config
+    );
+    let response = await request.text();
+    if (request.status <= 201) {
+      console.log("sent", response);
+    } else {
+      console.log(request.status, response);
+      throw new Error("Message not sent");
+    }
+    return Response.json(response);
   } catch (e) {
     console.log(e, "not sent");
     return new Response("error", { status: 500 });
